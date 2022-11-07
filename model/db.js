1201a2836a7166697f946cd7ea0688570010cf6e
Victor Gabriel <vgbp21@gmail.com>
2022-11-07 10:26:30 -0300

    atualização informações db

--- model/db.js
+++ model/db.js
@@ -1,15 +1,16 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-  "instapet",
-  "Victor_admin",
-  "useradmin1",
-  {
-    host: "instapet.cd52n0qtge9x.us-east-1.rds.amazonaws.com",
-    port: 5432,
-    dialect: "postgres",
-  }
-);
+const name = process.env.DB_NAME;
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+
+export const sequelize = new Sequelize(name, user, pass, {
+  host: host,
+  port: port,
+  dialect: "postgres",
+});
 try {
   await sequelize.authenticate();
   console.log("Connection successfully.");
