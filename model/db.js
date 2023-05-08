3f99290893731a83a8732be7c06820b1dee9d703
Victor Gabriel <vgbp21@gmail.com>
2023-05-08 08:27:39 -0300

    db

--- model/db.js
+++ model/db.js
@@ -9,11 +9,20 @@ const user = process.env.DB_USER;
 const pass = process.env.DB_PASS;
 const port = process.env.DB_PORT;
 
-export const sequelize = new Sequelize(database, user, pass, {
-  host: host,
-  port: port,
-  dialect: "postgres",
-});
+export const sequelize = new Sequelize(
+  "postgres://gbrvfgri:TUJDCq5QgatCUQNuLEDt_OSC73ZGaJJD@motty.db.elephantsql.com/gbrvfgri"
+);
+// const host = process.env.DB_HOST;
+// const database = process.env.DB_DATABASE;
+// const user = process.env.DB_USER;
+// const pass = process.env.DB_PASS;
+// const port = process.env.DB_PORT;
+
+// export const sequelize = new Sequelize(database, user, pass, {
+//   host: host,
+//   port: port,
+//   dialect: "postgres",
+// });
 
 try {
   await sequelize.authenticate();
