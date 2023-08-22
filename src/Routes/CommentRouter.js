24ce16eee1a37a1720c06556052d8247b14968bb
Victor Gabriel <vgbp21@gmail.com>
2023-08-22 20:29:10 -0300

    refactory

--- src/Routes/CommentRouter.js
+++ src/Routes/CommentRouter.js
@@ -1,28 +1,23 @@
 import express from "express";
 import verifytoken from "../middlewares/verifytoken.js";
-import {
-  createNewCommentary,
-  deleteCommentary,
-  getOneCommentaryById,
-  updateCommentary,
-} from "../Controllers/CommentController.js";
+import * as CommentControllerJs from "../Controllers/CommentController.js";
 
 const commentRouter = express.Router();
 
 commentRouter.post("/comment", verifytoken, (request, response) =>
-  createNewCommentary(request, response)
+  CommentControllerJs.createNewCommentary(request, response)
 );
 
 commentRouter.get("/comment/:commentId", (request, response) =>
-  getOneCommentaryById(request, response)
+  CommentControllerJs.getOneCommentaryById(request, response)
 );
 
 commentRouter.put("/comment", verifytoken, (request, response) =>
-  updateCommentary(request, response)
+  CommentControllerJs.updateCommentary(request, response)
 );
 
 commentRouter.delete("/comment/:commentId", verifytoken, (request, response) =>
-  deleteCommentary(request, response)
+  CommentControllerJs.deleteCommentary(request, response)
 );
 
 export default commentRouter;
--- src/Routes/FollowingRouter.js
+++ src/Routes/FollowingRouter.js
@@ -1,25 +1,21 @@
 import express from "express";
 import verifytoken from "../middlewares/verifytoken.js";
-import {
-  follow,
-  getAllUserFollowings,
-  unfollow,
-} from "../Controllers/FollowingController.js";
+import * as FollowingControllerJs from "../Controllers/FollowingController.js";
 
 const followersRouter = express.Router();
 
 followersRouter.post("/follow/:followId", verifytoken, (request, response) =>
-  follow(request, response)
+  FollowingControllerJs.follow(request, response)
 );
 
 followersRouter.get("/user/:userId/following", (request, response) =>
-  getAllUserFollowings(request, response)
+  FollowingControllerJs.getAllUserFollowings(request, response)
 );
 
 followersRouter.delete(
   "/following/:followId",
   verifytoken,
-  (request, response) => unfollow(request, response)
+  (request, response) => FollowingControllerJs.unfollow(request, response)
 );
 
 export default followersRouter;
